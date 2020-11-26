6f8c59f4851479b2af2a00b82796320debaa8eb9
Chris Whitehouse <chris_whitehouse@hotmail.com>
2020-11-26 15:44:41 +0000

    11. Using instance variables

--- lib/docking_station.rb
+++ lib/docking_station.rb
@@ -1,7 +1,14 @@
 require './lib/bike.rb'
 
-class DockingStation 
+class DockingStation
+  attr_reader :bike
+
   def release_bike
     Bike.new
   end
+
+  def dock_bike(bike)
+    @bike = bike
+  end
+
 end
--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -1,11 +1,35 @@
 require "../lib/docking_station"
 
 describe DockingStation do
-  it { is_expected.to respond_to(:release_bike)} 
-  
+
+  it 'releases a bike' do
+    is_expected.to respond_to(:release_bike)
+  end
+
   it 'gets a bike' do
     expect(subject.release_bike).to be_instance_of Bike
-  end 
+  end
+
+  it 'responds to the dock_bike method with one argument' do
+    is_expected.to respond_to(:dock_bike).with(1).argument
+  end
+
+  it 'accepts a bike object' do
+    docking_station = DockingStation.new
+    bike = docking_station.release_bike
+    expect(docking_station.dock_bike(bike)).to be_instance_of Bike
+  end
+
+  it 'responds to bike method' do
+    is_expected.to respond_to(:bike)
+  end
+
+  it 'allows user to see bike' do
+    docking_station = DockingStation.new
+    bike = docking_station.release_bike
+    docking_station.dock_bike(bike)
+    expect(docking_station.bike).to eq bike
+  end
+
+
 end
-
-
